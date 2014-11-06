fb4c789509765c79f9e8f94fbb9d6587cb8d0c97
Sheldon Hearn <sheldonh@starjuice.net>
2014-11-06 08:37:19 +0200

    Clarify the declarative/imperative split

--- features/support/hooks/use_secrets.rb
+++
@@ -1,5 +0,0 @@
-require_relative '../test_runners/stub_secret_store_test_runner'
-
-Before('@use_secrets') do
-  @test = SecretStore::Test::StubSecretStoreTestRunner.new
-end
--- features/test_runners/stub/hooks.rb
+++ features/test_runners/stub/hooks.rb
@@ -1,5 +1,7 @@
-require_relative '../test_runners/stub_secret_store_test_runner'
-
 Before('@manage_secrets') do
+  @test = SecretStore::Test::StubSecretStoreTestRunner.new
+end
+
+Before('@use_secrets') do
   @test = SecretStore::Test::StubSecretStoreTestRunner.new
 end
